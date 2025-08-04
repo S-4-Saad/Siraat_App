a637053829a45759bab7631ca0f63598d6e3d697
S-4-Saad <saadwaseem094@gmail.com>
2025-08-04 21:11:51 +0500

    Initial commit of Siraat App

---
+++ android/app/src/main/kotlin/com/example/siraat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.siraat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:siraat/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Siraat',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
